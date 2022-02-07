f8aadce6cfec9870468adbc8e4767c265335e478
sophie <sophie1620@gmail.com>
2022-02-07 00:24:41 -0500

    added new menu icon change when clicked

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// change menu icon
+const burgerIcon = document.querySelector('label')
+console.log(burgerIcon);
+
+burgerIcon.addEventListener('click', function(e){
+    // console.log(e);
+
+    console.log(e.target);
+
+    if (e.target.tagName === "I") {
+        closeIcon(e.target);
+        // NEED TO RMB TO PASS IN ARGUMENT!
+    }
+})
+
+function closeIcon(iconUpdate) {
+
+    iconUpdate.classList.toggle('fa-bars');
+    iconUpdate.classList.toggle('fa-times');       
+}
+
+
 // form submission response for .messageForm
     // when user submits* the form
         // display a thank you message (toggle the display between the form and this message?)
@@ -5,7 +27,7 @@
 
     // 1. identify/target the item
 const messageSubmission = document.querySelector('form');
-    console.log(messageSubmission);
+    // console.log(messageSubmission);
 
     // 2. add event listener
 messageSubmission.addEventListener('submit', function(e){
@@ -24,16 +46,52 @@ messageSubmission.addEventListener('submit', function(e){
     const userMessage = document.querySelector('textarea');
     // console.log(userMessage);
     const message = userMessage.value;
-
+    
     if (name && email && message) {
-        messageSubmission.innerHTML = '<p class="thnx blogThnkUMsg">Thank you for the submission!</p>'
+        messageSubmission.innerHTML = '<p class="thnx blogThnkUMsg">Thank you for the submission!</p>';
+        
     } else {
-        alert("Please complete the form.")
+        alert("Please complete the form.");
     } 
 });
 
 
 
-// post comment response as it is submitted? ~ look at the to-do list codealong
-    // userInput held in an empty string
-    // target ul element and append it as the last child
+// appending comment into comment part of blog.html
+// const commentPosting = document.getElementById('commentContainer');
+// // console.log(commentPosting);
+
+// commentPosting.addEventListener('submit', function(e){
+//     console.log(e);
+
+
+
+// })
+// function updateComment() {
+//     const userName = document.getElementById('name');
+//     // console.log(userName);
+//     const name = userName.value;
+//     // console.log(name);
+
+//     const userEmail = document.getElementById('email');
+//     const email = userEmail.value;
+
+//     const userMessage = document.querySelector('textarea');
+//     // console.log(userMessage);
+//     const message = userMessage.value;
+
+//     const listElement = document.createElement('li');
+//         // console.log(listElement);
+
+
+//     const paragraphElement = document.createElement('p');
+//     paragraphElement.textContent = message;
+//         // console.log(paragraphElement.textContent);
+        
+//     listElement.appendChild(paragraphElement);
+//         // console.log('listElement');
+
+//     const ulElement = document.getElementById('commentContainer');
+//         // // console.log(ulElement);
+//     ulElement.appendChild('listElement');
+// }
